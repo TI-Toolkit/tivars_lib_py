cd3740200784fb5c56e6c452d9f9a267f88ba0b3
Kevin Gomez <kgscience@hotmail.com>
2023-01-02 12:49:53 -0600

    Added newer models and features

--- tivars/models/__init__.py
+++ tivars/models/__init__.py
@@ -18,23 +18,47 @@ class TIFeatures:
     COLOR = 1 << 5
     EZ80 = 1 << 6
     EXACT_MATH = 1 << 7
+    PYTHON = 1 << 8
 
 
-features = [TIFeatures.NONE, TIFeatures.DEFAULT, TIFeatures.COMPLEX, TIFeatures.FLASH,
-            TIFeatures.APPS, TIFeatures.CLOCK, TIFeatures.COLOR, TIFeatures.EZ80, TIFeatures.EXACT_MATH]
+features = [TIFeatures.NONE, TIFeatures.DEFAULT, TIFeatures.COMPLEX, TIFeatures.FLASH, TIFeatures.APPS,
+            TIFeatures.CLOCK, TIFeatures.COLOR, TIFeatures.EZ80, TIFeatures.EXACT_MATH, TIFeatures.PYTHON]
 
 
-TI_82 = TIModel("82", flags82 := TIFeatures.DEFAULT, "**TI82**")
-TI_83 = TIModel("83", flags83 := flags82 | TIFeatures.COMPLEX, "**TI83**")
-TI_82a = TIModel("82a", flags82a := flags83 | TIFeatures.FLASH, "**TI83F*")
-TI_83p = TIModel("83+", flags83p := flags82a | TIFeatures.APPS, "**TI83F*")
-TI_84p = TIModel("84+", flags84p := flags83p | TIFeatures.CLOCK, "**TI83F*")
-TI_84pcse = TIModel("84+CSE", flags84pcse := flags84p | TIFeatures.COLOR, "**TI83F*")
-TI_84pce = TIModel("84+CE", flags84pce := flags84pcse | TIFeatures.EZ80, "**TI83F*")
-TI_83pce = TIModel("83PCE", flags83pce := flags84pce | TIFeatures.EXACT_MATH, "**TI83F*")
+flags82 = 0 | TIFeatures.DEFAULT
+flags83 = flags82 | TIFeatures.COMPLEX
+flags82a = flags83 | TIFeatures.FLASH
+flags83p = flags82a | TIFeatures.APPS
+flags84p = flags83p | TIFeatures.CLOCK
+flags84pcse = flags84p | TIFeatures.COLOR
+flags84pce = flags84pcse | TIFeatures.EZ80
+flags83pce = flags84pce | TIFeatures.EXACT_MATH
+flags83pceep = flags83pce | TIFeatures.PYTHON
+flags84pcepy = flags84pce | TIFeatures.PYTHON
+flags82aep = flags83pceep & ~TIFeatures.APPS
 
-models = [TI_82, TI_83, TI_82a, TI_83p, TI_84p, TI_84pcse, TI_84pce, TI_83pce]
+TI_82 = TIModel("82", flags82, "**TI82**")
+TI_83 = TIModel("83", flags83, "**TI83**")
+TI_82a = TIModel("82A", flags82a, "**TI83F*")
+TI_82p = TIModel("82+", flags83p, "**TI83F*")
+TI_83p = TIModel("83+", flags83p, "**TI83F*")
+TI_84p = TIModel("84+", flags84p, "**TI83F*")
+TI_84t = TIModel("84+T", flags84p, "**TI83F*")
+TI_84pcse = TIModel("84+CSE", flags84pcse, "**TI83F*")
+TI_84pce = TIModel("84+CE", flags84pce, "**TI83F*")
+TI_84pcet = TIModel("84+CET", flags84pce, "**TI83F*")
+TI_84pcetpy = TIModel("84+CETPE", flags84pcepy, "**TI83F*")
+TI_84pcepy = TIModel("84+CEPy", flags84pcepy, "**TI83F*")
+TI_83pce = TIModel("83PCE", flags83pce, "**TI83F*")
+TI_83pceep = TIModel("83PCEEP", flags83pceep, "**TI83F*")
+TI_82aep = TIModel("82AEP", flags82aep, "**TI83F*")
+
+models = [TI_82, TI_83, TI_82a, TI_82p, TI_83p,
+          TI_84p, TI_84t, TI_84pcse, TI_84pce, TI_84pcet, TI_84pcetpy, TI_84pcepy,
+          TI_83pce, TI_83pceep, TI_82aep]
 
 
-__all__ = ["TI_82", "TI_83", "TI_82a", "TI_83p", "TI_84p", "TI_84pcse", "TI_84pce", "TI_83pce", "models",
-           "TIFeatures", "features", "TIModel"]
+__all__ = ["TI_82", "TI_83", "TI_82a", "TI_83p",
+           "TI_84p", "TI_84t", "TI_84pcse", "TI_84pce", "TI_83pce", "TI_84pcet", "TI_84pcetpy", "TI_84pcepy",
+           "TI_83pce", "TI_83pceep", "TI_82aep",
+           "models", "TIFeatures", "features", "TIModel"]
