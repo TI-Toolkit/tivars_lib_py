c732e002d4322db0970cbbed6c011b5d400ab8e4
Kevin Gomez <kgscience@hotmail.com>
2023-01-02 00:58:40 -0600

    Evil backslashes are evil

--- src/tokenizer/tokens/__init__.py
+++ src/tokenizer/tokens/__init__.py
@@ -1,16 +1,16 @@
 from ..parse import load_tokens_xml
 
-AXE_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\Axe.xml")
+AXE_TOKENS = load_tokens_xml("./src/tokenizer/tokens/Axe.xml")
 
-CE_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\TI-84+CE.xml")
+CE_TOKENS = load_tokens_xml("./src/tokenizer/tokens/TI-84+CE.xml")
 
-CSE_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\TI-84+CSE.xml")
+CSE_TOKENS = load_tokens_xml("./src/tokenizer/tokens/TI-84+CSE.xml")
 
-GRAMMER_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\Grammer.xml")
+GRAMMER_TOKENS = load_tokens_xml("./src/tokenizer/tokens/Grammer.xml")
 
-NOLIB_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\NoLib.xml")
+NOLIB_TOKENS = load_tokens_xml("./src/tokenizer/tokens/NoLib.xml")
 
-PRIZM_TOKENS = load_tokens_xml(".\\tokenizer\\tokens\\Prizm.xml")
+PRIZM_TOKENS = load_tokens_xml("./src/tokenizer/tokens/Prizm.xml")
 
 
 __all__ = ["AXE_TOKENS", "CE_TOKENS", "CSE_TOKENS", "GRAMMER_TOKENS", "NOLIB_TOKENS", "PRIZM_TOKENS"]
