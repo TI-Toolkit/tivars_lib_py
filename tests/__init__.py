dd70190b450c53b82082f82d6123576747a21ead
Kevin Gomez <kgscience@hotmail.com>
2023-01-12 16:32:30 -0500

    Add more tests. Thanks Adriweb!

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,12 +4,30 @@ from tivars.models import *
 from tivars.types import *
 
 
+class VarTests(unittest.TestCase):
+    def test_all_attributes(self):
+        test_var = TIProgram()
+        test_var.open("tests/data/Program.8xp")
+
+        self.assertEqual(test_var.magic, "**TI83F*")
+        self.assertEqual(test_var.extra, b'\x1A\x0A')
+        self.assertEqual(test_var.product_id, b'\x0A')
+        self.assertEqual(test_var.comment, "Created by TI Connect CE 5.1.0.68")
+
+        self.assertEqual(test_var.meta_length, TIVar.flash_meta_length)
+        self.assertEqual(test_var.name, "SETDATE")
+        self.assertEqual(test_var.version, b'\x04')
+        self.assertEqual(test_var.archived, False)
+
+        self.assertEqual(test_var.header.entry_length, test_var.entry_length)
+
+
 class TokenizationTests(unittest.TestCase):
     def test_load_from_file(self):
         test_program = TIProgram()
+        test_program.open("tests/data/Program.8xp")
 
-        test_program.open("tests/HELLO.8xp")
-        with open("tests/HELLO.8xp", 'rb') as file:
+        with open("tests/data/Program.8xp", 'rb') as file:
             self.assertEqual(test_program.bytes(), file.read())
             file.seek(0)
 
@@ -18,18 +36,20 @@ class TokenizationTests(unittest.TestCase):
             self.assertEqual(test_program.bytes(), file.read())
 
     def test_load_from_string(self):
-        test_program = TIProgram(name="HELLO", model=TI_84P, default_product_id=True)
+        test_program = TIProgram(name="SETDATE", model=TI_84P)
+        test_program.comment = "Created by TI Connect CE 5.1.0.68"
+        test_program.version = b'\x04'
 
-        test_program.load_string(string := "Disp \"HELLO WORLD!\"")
+        test_program.load_string(string := "setDate(1")
 
-        with open("tests/HELLO.8xp", 'rb') as file:
+        with open("tests/data/Program.8xp", 'rb') as file:
             self.assertEqual(test_program.bytes(), file.read())
             self.assertEqual(test_program.string(), string)
 
-    def test_lots_of_chars(self):
+    def test_all_tokens(self):
         test_program = TIProgram()
 
-        with open("tests/CHARS2.8xp", 'rb') as file:
+        with open("tests/data/ALLTOKS.8Xp", 'rb') as file:
             test_program.load(file)
             file.seek(0)
 
@@ -38,9 +58,9 @@ class TokenizationTests(unittest.TestCase):
     def test_save_to_file(self):
         test_program = TIProgram()
 
-        test_program.open("tests/HELLO.8xp")
-        test_program.save("tests/HELLO2.8xp")
+        test_program.open("tests/data/Program.8xp")
+        test_program.save("tests/data/Program_new.8xp")
 
-        with open("tests/HELLO.8xp", 'rb') as orig:
-            with open("tests/HELLO2.8xp", 'rb') as new:
+        with open("tests/data/Program.8xp", 'rb') as orig:
+            with open("tests/data/Program_new.8xp", 'rb') as new:
                 self.assertEqual(new.read(), orig.read())
