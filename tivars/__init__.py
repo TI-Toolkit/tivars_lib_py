043e7dcbfb9502c40a0ff7ac11ac5bb930e8c4c2
KG <kgscience@hotmail.com>
2023-08-26 00:39:50 -0400

    Flatten submodules for wildcard import

--- tivars/__init__.py
+++ tivars/__init__.py
@@ -1,6 +1,7 @@
 from .tokenizer import *
 from .types import *
 from .models import *
-
-from .flags import *
 from .var import *
+
+
+__all__ = list(set(tokenizer.__all__) | set(types.__all__) | set(models.__all__) | set(var.__all__))
