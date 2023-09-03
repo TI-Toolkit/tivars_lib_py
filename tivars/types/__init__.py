63ce50e4874dfc990850ceb33562c84ace30a353
KG <kgscience@hotmail.com>
2023-09-03 12:50:54 -0400

    Reworkaround

--- tivars/types/__init__.py
+++ tivars/types/__init__.py
@@ -13,26 +13,27 @@ from .tokenized import *
 from ..var import TIEntry
 
 
-__all__ = ["TIEntry",
-           "TIReal",
-           "TIRealList", "TIMatrix",
-           "TIEquation", "TIString", "TIProgram", "TIProtectedProgram",
-           "TIPicture", "TIMonoPicture",
-           "TIMonoGDB", "TIGDB", "TIGraphedEquation",
-           "TIMonoFuncGDB", "TIMonoParamGDB", "TIMonoPolarGDB", "TIMonoSeqGDB",
-           "TIFuncGDB", "TIParamGDB", "TIPolarGDB", "TISeqGDB",
-           "EquationFlags", "GraphMode", "GraphStyle", "GraphColor", "GlobalLineStyle",
-           "TIComplex", "TIComplexList",
-           "TIUndefinedReal",
-           "TIWindowSettings", "TIRecallWindow", "TITableSettings",
-           "TIAppVar",
-           "TIRealFraction",
-           "TIImage",
-           "TIComplexFraction",
-           "TIRealRadical", "TIComplexRadical",
-           "TIComplexPi", "TIComplexPiFraction",
-           "TIRealPi", "TIRealPiFraction"
-           ]
-
 if TYPE_CHECKING:
     __all__ = []
+
+else:
+    __all__ = ["TIEntry",
+               "TIReal",
+               "TIRealList", "TIMatrix",
+               "TIEquation", "TIString", "TIProgram", "TIProtectedProgram",
+               "TIPicture", "TIMonoPicture",
+               "TIMonoGDB", "TIGDB", "TIGraphedEquation",
+               "TIMonoFuncGDB", "TIMonoParamGDB", "TIMonoPolarGDB", "TIMonoSeqGDB",
+               "TIFuncGDB", "TIParamGDB", "TIPolarGDB", "TISeqGDB",
+               "EquationFlags", "GraphMode", "GraphStyle", "GraphColor", "GlobalLineStyle",
+               "TIComplex", "TIComplexList",
+               "TIUndefinedReal",
+               "TIWindowSettings", "TIRecallWindow", "TITableSettings",
+               "TIAppVar",
+               "TIRealFraction",
+               "TIImage",
+               "TIComplexFraction",
+               "TIRealRadical", "TIComplexRadical",
+               "TIComplexPi", "TIComplexPiFraction",
+               "TIRealPi", "TIRealPiFraction"
+               ]
